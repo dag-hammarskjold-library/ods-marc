77be490b0180d37a4bb6f3f6ed9f0a9615fcdf85
jbukhari <joe.bukhari@gmail.com>
2020-08-12 12:32:36 -0400

    dev

--- ods-marc.py
+++ ods-marc.py
@@ -1,6 +1,6 @@
 """Converts an Excel export of ODS data to MARC for import into Horizon"""
 
-import sys, os
+import sys, os, logging
 from argparse import ArgumentParser
 from dlx import DB
 from dlx.util import Table
@@ -8,48 +8,60 @@ from dlx.marc import Bib, BibSet, Auth, QueryDocument, Condition
 
 parser = ArgumentParser()
 parser.add_argument('--connect', required=True)
-parser.add_argument('--file', required=True)
+parser.add_argument('--input_file', required=True)
+parser.add_argument('--output_file', required=True)
+parser.add_argument('--output_format', choices =['mrc', 'mrk'])
 args = parser.parse_args()
 
 DB.connect(args.connect)
+output_handle = open(args.output_file, 'w')
 
 def main():
-    tbl = Table.from_excel(args.file, date_format='%Y%m%d')
+    tbl = Table.from_excel(args.input_file, date_format='%Y%m%d')
     bibs = BibSet()
     
     for temp_id in tbl.index.keys():
         bib = Bib()
 
         for field_name in tbl.index[temp_id].keys():
-            instance = 0
             value = tbl.index[temp_id][field_name]
             
             if field_name == 'Doc Symbol':
                 bib.set('191', 'a', value)
             elif field_name == 'Title':
+                if not value:
+                    logging.warn('Title not found for row {}'.format(temp_id + 2))
+                    
                 bib.set('245', 'a', value)
             elif field_name == 'publicaion date':
                 bib.set('269', 'a', value)
             elif field_name == 'Job Number':
-                bib.set('029', 'a', value)
+                for job in value.split(';'):
+                    bib.set('029', 'a', job, address=['+'])
             elif field_name == 'Lang available':
-                pass
-                # todo
-            elif field_name =='Tcodes':
-                codes = value.split(';')
+                langtext = ''
                 
-                for code in codes:
-                    q = QueryDocument(Condition('035', {'a': code}))  
+                for lang in sorted(value.split(' ')):
+                    langtext += {'AP': 'ara', 'EP': 'eng'}[lang]
+                    
+                bib.set('041', 'a', langtext)    
+            elif field_name =='Tcodes':
+                for tcode in value.split(';'):
+                    q = QueryDocument(Condition('035', {'a': tcode}))
                     auth = Auth.find_one(q.compile())
                     
                     if auth:
-                        bib.set('650', 'a', auth.id)
+                        bib.set('650', 'a', auth.id, address=['+'])
                     else:
-                        raise Exception('Auth record for code "{}" not found'.format(code))
-                
+                        logging.warning('Auth record for Tcode "{}" not found'.format(tcode))
+                        
+        bib.set_008()
         bibs.records.append(bib)
-        
-    print(bibs.to_mrc())
+    
+        if args.output_format == 'mrk':
+            output_handle.write(bibs.to_mrk())
+        else:
+            output_handle.write(bib.to_mrc())
     
 ###
 
